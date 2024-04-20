88f7cd4fd0336c6a43c4223345e95908ddb0fba3
Vicente <busslina@gmail.com>
2024-04-20 19:21:04 +0200

    snapshot

--- test/rearch_weird_behaviour_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:rearch_weird_behaviour/rearch_weird_behaviour.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
